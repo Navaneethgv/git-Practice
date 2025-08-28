5b869f324ed89afa830942abbb9cf4d20d88fdce
Navaneeth <navaneethgvn@gmail.com>
2025-08-28 20:48:07 +0530

    Update NewFile.java

--- NewFile.java
+++ NewFile.java
@@ -1,5 +1,5 @@
 public class NewFile {
     public static void main(String[] args) {
-        System.out.println("Newly file created for the Git practice");
+        System.out.println("file Created for the Git Practice");
     }
 }
